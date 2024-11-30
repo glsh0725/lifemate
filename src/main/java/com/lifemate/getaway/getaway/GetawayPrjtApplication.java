23a41eefe58c04213d8aa293b0b24d2f05fd07a7
glsh0725 <lsh920725@naver.com>
2024-12-01 06:32:47 +0900

    init

---
+++ src/main/java/com/lifemate/getaway/getaway/GetawayPrjtApplication.java
@@ -0,0 +1,13 @@
+package com.lifemate.getaway.getaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetawayPrjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetawayPrjtApplication.class, args);
+	}
+
+}
