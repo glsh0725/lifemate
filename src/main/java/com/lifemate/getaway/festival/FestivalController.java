387e18dd99e69005a9970ceed499037bdfc2f9c7
seohyeon1210 <seohyeon174@naver.com>
2024-12-04 09:45:45 +0900

    init

--- src/main/java/com/lifemate/getaway/festival/FestivalController.java
+++ src/main/java/com/lifemate/getaway/festival/FestivalController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public interface FestivalController {
 
+    // 인터페이스는 추상 메소드를 가질 수 없음 Impl로 옮겨야 함.
 /*    @GetMapping("/festival")
     public String festivalPage() {
         return "festival/festival";
--- src/main/java/com/lifemate/getaway/festival/FestivalControllerImpl.java
+++ src/main/java/com/lifemate/getaway/festival/FestivalControllerImpl.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public class FestivalControllerImpl implements FestivalController {
 
+    // 컨트롤러 옮김
     @GetMapping("/festival")
     public String festivalPage() {
         return "festival/festival";
