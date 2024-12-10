0bfdbbd9dcf22519a15547cfe5d4d75e507a25ef
HyunSeopPark <qkrgustjq123@naver.com>
2024-12-10 17:25:56 +0900

    임의 업데이트 출력안됨 수정필요함
    죽어!!!!!!!!!!!!!!!JSP!!!!!!!!!!!!

--- src/main/java/fs/four/dropout/community/controller/CommunityControllerImpl.java
+++ src/main/java/fs/four/dropout/community/controller/CommunityControllerImpl.java
@@ -3,9 +3,11 @@ package fs.four.dropout.community.controller;
 import fs.four.dropout.community.service.CommunityService;
 import fs.four.dropout.community.vo.CommunityVO;
 import fs.four.dropout.community.vo.CommunityWriteVO;
+import fs.four.dropout.community.vo.CommunityViewVO;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -35,11 +37,18 @@ public class CommunityControllerImpl implements CommunityController {
 
         mav.addObject("postsList", postsList);
         return mav;
-
     }
 
     @GetMapping("/community/save")
     public String communitySave() {
         return "community/save";
+    }
+
+    // 게시글 상세 보기 기능 추가
+    @GetMapping("/community/view")
+    public ModelAndView viewPost(@RequestParam("postId") Number postId) {
+        ModelAndView mav = new ModelAndView("community/communityview");  // views/community/communityview.jsp
+        mav.addObject("postId", postId);
+        return mav;
     }
 }
